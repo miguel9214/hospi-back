5e856e6f22a40e9fa0c0d26f33a9c8e2bbfd8910
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2024-08-27 16:41:27 -0500

    filtro mejorado para usuarios

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,23 @@
+// middlewares/adminMiddleware.js
+
 const jwt = require('jsonwebtoken');
-require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
+const { User, Role } = require('../models');
 
-const JWT_SECRET = process.env.JWT_SECRET; // Obtener la clave secreta desde las variables de entorno
+const adminMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-// Middleware para verificar el token JWT
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Se espera que el token esté en el formato "Bearer TOKEN"
+    const user = await User.findByPk(decoded.id, { include: Role });
+    if (user && user.Role.name === 'Admin') {
+      req.user = user;
+      next();
+    } else {
+      return res.status(403).json({ error: 'Acceso denegado: No eres administrador' });
+    }
+  } catch (error) {
+    return res.status(401).json({ error: 'Autenticación fallida' });
+  }
+};
 
-  if (token == null) return res.sendStatus(401); // Si no hay token, se devuelve un error 401
-
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // Si el token no es válido, se devuelve un error 403
-
-    req.user = user; // Guarda la información del usuario en `req.user`
-    next(); // Pasa al siguiente middleware o ruta
-  });
-}
-
-module.exports = authenticateToken;
+module.exports = adminMiddleware;
